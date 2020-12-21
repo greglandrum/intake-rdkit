dcdce226be8a5a8c42144de2a62cd6cdb6d13472
greg landrum <greg.landrum@gmail.com>
2020-12-21 09:52:38 +0100

    robustify testing

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -8,37 +8,25 @@
 import unittest
 import intake
 from rdkit import Chem
+from pathlib import Path
 
 
 class TestCase(unittest.TestCase):
     def test_smiles(self):
-        ds = intake.open_smiles('./data/first_200.tpsa.csv',
-                                header=None,
-                                smilesColumn=0)
-        df = ds.read()
-        self.assertEqual(len(df), 200)
-        self.assertEqual(Chem.MolToSmiles(df.iloc[0]['mol']),
-                         'CC1=CC(=O)C=CC1=O')
-        self.assertEqual((df.iloc[0][1]), 34.14)
-
-        ds = intake.open_smiles('./data/first_200.tpsa.csv.gz',
-                                header=None,
-                                smilesColumn=0)
-        df = ds.read()
-        self.assertEqual(len(df), 200)
-        self.assertEqual(Chem.MolToSmiles(df.iloc[0]['mol']),
-                         'CC1=CC(=O)C=CC1=O')
-        self.assertEqual((df.iloc[0][1]), 34.14)
+        for fn in ("data/first_200.tpsa.csv", "data/first_200.tpsa.csv.gz"):
+            fn = Path(__file__).parent / fn
+            ds = intake.open_smiles(str(fn), header=None, smilesColumn=0)
+            df = ds.read()
+            self.assertEqual(len(df), 200)
+            self.assertEqual(Chem.MolToSmiles(df.iloc[0]['mol']),
+                             'CC1=CC(=O)C=CC1=O')
+            self.assertEqual((df.iloc[0][1]), 34.14)
 
     def test_sdf(self):
-        ds = intake.open_sdf('./data/first_200.props.sdf')
-        df = ds.read()
-        self.assertEqual(len(df), 200)
-        self.assertEqual(Chem.MolToSmiles(df.iloc[0]['mol']),
-                         'CC1=CC(=O)C=CC1=O')
-
-        ds = intake.open_sdf('./data/first_200.props.sdf.gz')
-        df = ds.read()
-        self.assertEqual(len(df), 200)
-        self.assertEqual(Chem.MolToSmiles(df.iloc[0]['mol']),
-                         'CC1=CC(=O)C=CC1=O')
+        for fn in ("data/first_200.props.sdf", "data/first_200.props.sdf.gz"):
+            fn = Path(__file__).parent / fn
+            ds = intake.open_sdf(str(fn))
+            df = ds.read()
+            self.assertEqual(len(df), 200)
+            self.assertEqual(Chem.MolToSmiles(df.iloc[0]['mol']),
+                             'CC1=CC(=O)C=CC1=O')
