c89c2cf0174e046cbe73f40df087edaacdce8abf
greg landrum <greg.landrum@gmail.com>
2021-12-27 06:32:03 +0100

    reformat, support "None" in catalog

--- intake_rdkit/sdf.py
+++ intake_rdkit/sdf.py
@@ -10,6 +10,8 @@ import pandas as pd
 import intake
 import numpy as np
 import gzip
+
+
 class SDFSource(intake.source.base.DataSource):
     " Inspired by example here: https://github.com/intake/intake-examples/blob/master/tutorial/data_engineer.ipynb "
     container = 'dataframe'
@@ -17,66 +19,67 @@ class SDFSource(intake.source.base.DataSource):
     version = '0.1.0'
     partition_access = True
     suppl_factory = Chem.SDMolSupplier
-    
+
     def __init__(self, filename, metadata=None, nPartitions=1, **kwargs):
-        super(SDFSource, self).__init__(
-            metadata=metadata
-        )
+        super(SDFSource, self).__init__(metadata=metadata)
         self._fname = filename
         if filename.endswith('.gz'):
-            self.suppl_factory = lambda fname,**kwargs:Chem.ForwardSDMolSupplier(gzip.open(fname),**kwargs)
+            self.suppl_factory = lambda fname, **kwargs: Chem.ForwardSDMolSupplier(
+                gzip.open(fname), **kwargs)
             if nPartitions != 1:
-                raise ValueError('can only read compressed files with a single partition')
+                raise ValueError(
+                    'can only read compressed files with a single partition')
+        for k in kwargs:
+            if kwargs[k] == 'None':
+                kwargs[k] = None
         self._supplkwargs = kwargs
-        # this is, hopefully, for future use:
         self._nPartitions = nPartitions
 
     def _get_schema(self):
         " reads property names from the first molecule and uses those "
         # FIX handle error if the first molecule doesn't exist
-        suppl = self.suppl_factory(self._fname,**self._supplkwargs)
+        suppl = self.suppl_factory(self._fname, **self._supplkwargs)
         m0 = next(suppl)
         pd = m0.GetPropsAsDict()
-        dt = {'mol':'O'}
-        for pn,pv in pd.items():
+        dt = {'mol': 'O'}
+        for pn, pv in pd.items():
             dt[pn] = np.dtype(type(pv)).name
-        return intake.source.base.Schema(
-            datashape=None,
-            dtype=dt,
-            shape=(None, len(dt)),
-            npartitions=self._nPartitions,
-            extra_metadata=self._supplkwargs
-        )
+        return intake.source.base.Schema(datashape=None,
+                                         dtype=dt,
+                                         shape=(None, len(dt)),
+                                         npartitions=self._nPartitions,
+                                         extra_metadata=self._supplkwargs)
 
     def _get_partition(self, i):
-        suppl = self.suppl_factory(self._fname,**self._supplkwargs)
+        suppl = self.suppl_factory(self._fname, **self._supplkwargs)
         res = []
-        if self._nPartitions>1:
-            partSize = len(suppl)//self._nPartitions
-            start = i*partSize
-            if i==self._nPartitions-1:
+        if self._nPartitions > 1:
+            partSize = len(suppl) // self._nPartitions
+            start = i * partSize
+            if i == self._nPartitions - 1:
                 end = len(suppl)
             else:
                 end = start + partSize
-                
-            for idx in range(start,end):
+
+            for idx in range(start, end):
                 m = suppl[idx]
                 if m is not None:
-                    row = {'mol':m}
+                    row = {'mol': m}
                     row.update(m.GetPropsAsDict())
                     res.append(row)
         else:
             for m in suppl:
                 if m is not None:
-                    row = {'mol':m}
+                    row = {'mol': m}
                     row.update(m.GetPropsAsDict())
                     res.append(row)
         return pd.DataFrame(res)
 
     def read(self):
         self._load_metadata()
-        return pd.concat([self.read_partition(i) for i in range(self.npartitions)])
-        
+        return pd.concat(
+            [self.read_partition(i) for i in range(self.npartitions)])
+
     def _close(self):
         # close any files, sockets, etc
-        self.suppl=None
+        self.suppl = None
--- intake_rdkit/smiles.py
+++ intake_rdkit/smiles.py
@@ -11,6 +11,7 @@ import intake
 import numpy as np
 import gzip
 
+
 class SmilesSource(intake.source.base.DataSource):
     " Inspired by example here: https://github.com/intake/intake-examples/blob/master/tutorial/data_engineer.ipynb "
     container = 'dataframe'
@@ -18,15 +19,21 @@ class SmilesSource(intake.source.base.DataSource):
     version = '0.1.0'
     partition_access = True
     suppl_factory = Chem.SmilesMolSupplier
-    
-    def __init__(self, filename, metadata=None, nPartitions=1, smilesColumn='smiles', 
-        useSmarts=False, **kwargs):
-        super(SmilesSource, self).__init__(
-            metadata=metadata
-        )
+
+    def __init__(self,
+                 filename,
+                 metadata=None,
+                 nPartitions=1,
+                 smilesColumn='smiles',
+                 useSmarts=False,
+                 **kwargs):
+        super(SmilesSource, self).__init__(metadata=metadata)
         self._fname = filename
         self._smilesColumn = smilesColumn
         self._useSmarts = useSmarts
+        for k in kwargs:
+            if kwargs[k] == 'None':
+                kwargs[k] = None
         self._supplkwargs = kwargs
         # this is, hopefully, for future use:
         self._nPartitions = nPartitions
@@ -34,36 +41,34 @@ class SmilesSource(intake.source.base.DataSource):
     def _get_schema(self):
         " reads property names from the first molecule and uses those "
         # FIX handle error if the first molecule doesn't exist
-        df = pd.read_csv(self._fname,nrows=1,**self._supplkwargs)
-        dt = {'mol':'O'}
-        for pn,pv in zip(df.columns,df.iloc[0]):
+        df = pd.read_csv(self._fname, nrows=1, **self._supplkwargs)
+        dt = {'mol': 'O'}
+        for pn, pv in zip(df.columns, df.iloc[0]):
             dt[pn] = np.dtype(type(pv)).name
         self._colNames = list(df.columns)
-        return intake.source.base.Schema(
-            datashape=None,
-            dtype=dt,
-            shape=(None, len(dt)),
-            npartitions=self._nPartitions,
-            extra_metadata=self._supplkwargs
-        )
+        return intake.source.base.Schema(datashape=None,
+                                         dtype=dt,
+                                         shape=(None, len(dt)),
+                                         npartitions=self._nPartitions,
+                                         extra_metadata=self._supplkwargs)
 
     def _get_partition(self, i):
-        df = pd.read_csv(self._fname,**self._supplkwargs)
-        partSize = len(df)//self._nPartitions
-        start = i*partSize
-        if i==self._nPartitions-1:
+        df = pd.read_csv(self._fname, **self._supplkwargs)
+        partSize = len(df) // self._nPartitions
+        start = i * partSize
+        if i == self._nPartitions - 1:
             end = len(df)
         else:
             end = start + partSize
-        res = [] 
-        for idx in range(start,end):
+        res = []
+        for idx in range(start, end):
             row = df.iloc[idx]
             if not self._useSmarts:
                 m = Chem.MolFromSmiles(row[self._smilesColumn])
             else:
                 m = Chem.MolFromSmarts(row[self._smilesColumn])
             if m is not None:
-                nrow = {'mol':m}
+                nrow = {'mol': m}
                 for k in self._colNames:
                     nrow[k] = row[k]
                 res.append(nrow)
@@ -71,8 +76,9 @@ class SmilesSource(intake.source.base.DataSource):
 
     def read(self):
         self._load_metadata()
-        return pd.concat([self.read_partition(i) for i in range(self.npartitions)])
-        
+        return pd.concat(
+            [self.read_partition(i) for i in range(self.npartitions)])
+
     def _close(self):
         # close any files, sockets, etc
-        self.suppl=None
+        self.suppl = None
